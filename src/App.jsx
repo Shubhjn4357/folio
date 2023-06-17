71d3b140549cf17cf4052c9629cf642970df35ea
Shubhjn4357 <shubhamjain.com.in@gmail.com>
2023-06-17 20:13:36 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Hero, Navbar, Tech, Works ,Footer} from "./components";
+import CustomCursor from "./components/CustomCursor";
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
+        <CustomCursor />
         <div className='effective-bg'>
           <Navbar />
           <Hero />
